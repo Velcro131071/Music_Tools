e2de778b8a78fdd97bdac1d89181b0c32cb33269
Velcro131071 <jamie.hocking.business@gmail.com>
2026-03-15 17:58:02 +1030

    Initialise index.html source.js styles.css

---
+++ source.js
@@ -0,0 +1,31 @@
+const musicKeys = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B'];
+const scaleTypes = ['Ionian', 'Dorian', 'Phyrgian', 'Lydian', 'Mixolydian', 'Aeolian', 'Locrian']
+const chordTypes = ['Major Triad', 'Minor Triad', 'Augmented Triad', 'Diminished Triad']
+
+let keyBtn = document.getElementById('keyBtn');
+let chordBtn = document.getElementById('chordBtn');
+let scaleBtn = document.getElementById('scaleBtn');
+
+let keyDisplayBox = document.getElementById('keyDisplayBox');
+let chordDisplayBox = document.getElementById('chordDisplayBox');
+let scaleDisplayBox = document.getElementById('scaleDisplayBox');
+
+let keyIndex = 0;
+
+
+keyBtn.addEventListener('click', function() {
+    keyIndex = Math.floor(Math.random() * musicKeys.length)
+    keyDisplayBox.textContent = musicKeys[keyIndex]
+});
+
+
+
+chordBtn.addEventListener('click', function() {
+    let chordIndex = Math.floor(Math.random() * chordTypes.length)
+    chordDisplayBox.textContent = musicKeys[chordIndex] + ' ' + chordTypes[chordIndex]
+});
+
+scaleBtn.addEventListener('click', function() {
+    let scaleIndex = Math.floor(Math.random() * scaleTypes.length)
+    scaleDisplayBox.textContent = musicKeys[keyIndex] + ' ' + scaleTypes[scaleIndex]
+});
